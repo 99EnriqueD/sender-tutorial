cb2801cfe3dd8f1a6718673317f47b5adb0f2baa
Enrique Dehaerne <47824387+99EnriqueD@users.noreply.github.com>
2021-04-08 13:47:14 +0200

    Add final code in main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+function measureWind () {
+    return input.lightLevel()
+}
+let measured_data = 0
+let team_name = "Team 1"
+radio.setGroup(5)
+radio.setTransmitPower(5)
 basic.forever(function () {
-	
+    measured_data = measureWind()
+    led.plotBarGraph(
+    measured_data,
+    255
+    )
+    radio.sendValue(team_name, measured_data)
 })
+
