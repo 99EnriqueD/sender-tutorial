a9b711404df4ef144a3b40e54cc9a23d1123d0b1
Enrique Dehaerne <47824387+99EnriqueD@users.noreply.github.com>
2021-04-10 16:54:48 +0200

    update code to work with propeller

--- main.ts
+++ main.ts
@@ -1,16 +1,55 @@
-function measureWind () {
-    return input.lightLevel()
+function calc_average () {
+    sum = 0
+    number_data_points = 1000
+    for (let index = 0; index < number_data_points; index++) {
+        sum += pins.analogReadPin(AnalogPin.P1)
+    }
+    return sum / number_data_points
 }
-let measured_data = 0
-let team_name = "Team 1"
+function send_fev1 (team_name: string, fev1: number) {
+    radio.sendValue("" + team_name + "." + "fev1", 0)
+    basic.pause(20)
+    radio.sendValue("" + team_name + "." + "fev1", fev1)
+    basic.pause(20)
+    radio.sendValue("" + team_name + "." + "fev1", 0)
+}
+function measure_propeller (team_name: string, ms: number) {
+    start_time = control.millis()
+    while (control.millis() < start_time + ms) {
+        average = calc_average()
+        radio_propeller(team_name, average)
+    }
+}
+function radio_propeller (team_name: string, value: number) {
+    radio.sendValue("" + team_name + "." + "propeller", value)
+}
+let average = 0
+let start_time = 0
+let number_data_points = 0
+let sum = 0
 radio.setGroup(5)
-radio.setTransmitPower(5)
+let threshold = 5
+let team_name = "Team1"
 basic.forever(function () {
-    measured_data = measureWind()
-    led.plotBarGraph(
-    measured_data,
-    255
-    )
-    radio.sendValue(team_name, measured_data)
+    if (pins.analogReadPin(AnalogPin.P1) >= threshold) {
+        measure_propeller(team_name, 500)
+        send_fev1(team_name, average)
+        measure_propeller(team_name, 2500)
+    }
 })
-
+control.inBackground(function () {
+    while (true) {
+        if (average < threshold) {
+            basic.showLeds(`
+                . # . . .
+                . # . # #
+                . . # . .
+                # # . # .
+                . . . # .
+                `)
+        } else {
+            basic.showIcon(IconNames.Yes)
+            basic.pause(1000)
+        }
+    }
+})
